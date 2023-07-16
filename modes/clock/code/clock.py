d1859c412b300c75d0e76623415a5a10a0e54bf2
Gill <johan@gill.nu>
2023-07-16 16:04:32 +0200

    Added date_variable so I can set a date as well.
    Added date_variable so I can set a date as well.

--- modes/clock/code/clock.py
+++ modes/clock/code/clock.py
@@ -7,12 +7,16 @@ class GetClock(Mode):
     def mode_start(self, **kwargs):
            # del kwargs
             self.add_mode_event_handler('set_clock', self._write_time)
+            self.add_mode_event_handler('set_date', self._write_date)
     
     def _write_time(self, **kwargs):
-            time_string = time.strftime("%H:%M") # %H:%M for Hour and Minutes, can change to whatever you need.
+            time_string = time.strftime("%H:%M")
             self.machine.variables.set_machine_var("time_variable", time_string) 
             self.machine.events.post("time_variable_saved")
-
+    def _write_date(self, **kwargs):
+            time_string = time.strftime("20%y.%m.%d")
+            self.machine.variables.set_machine_var("date_variable", time_string) 
+            self.machine.events.post("date_variable_saved")
 
 
 
